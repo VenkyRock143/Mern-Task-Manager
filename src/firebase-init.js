683be3bf9a5d4c830b5fb4bd1f884bd27a6b22bb
VenkyRock143 <venky.balusani1@gmail.com>
2024-06-06 23:55:54 +0530

    firebase

--- src/firebase-init.js
+++ src/firebase-init.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxrz0iH8qtwP3iwLP0lmdRmWEYaFZMKno",
-  authDomain: "blogs-ef0e2.firebaseapp.com",
-  projectId: "blogs-ef0e2",
-  storageBucket: "blogs-ef0e2.appspot.com",
-  messagingSenderId: "425017987434",
-  appId: "1:425017987434:web:9a8650db67e72269969513"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
