36da1f008851cf0de12ef4911e834547af305953
VenkyRock143 <venky.balusani1@gmail.com>
2024-07-22 11:39:14 +0530

    removed frontend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const path = require('path');
+// const path = require('path');
 
 dotenv.config();
 connectDB();
@@ -14,13 +14,13 @@ app.use(express.json());
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/build')));
   
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
-    );
-}
+//     app.get('*', (req, res) =>
+//       res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
+//     );
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
